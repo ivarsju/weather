20786837a41adbdb55bf445488e4771b9da56690
ivars <ivars.ju@gmail.com>
2022-10-10 13:40:51 +0300

    Added weather entity

---
+++ src/Entity/Weather.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\WeatherRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: WeatherRepository::class)]
+#[ORM\UniqueConstraint(name: 'city_date', columns: ['city', 'date'])]
+class Weather
+{
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $temperature = null;
+
+    #[ORM\Column]
+    private ?int $windSpeed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?int
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(int $temperature): self
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getWindSpeed(): ?int
+    {
+        return $this->windSpeed;
+    }
+
+    public function setWindSpeed(int $windSpeed): self
+    {
+        $this->windSpeed = $windSpeed;
+
+        return $this;
+    }
+}
